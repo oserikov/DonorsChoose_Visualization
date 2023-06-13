03c6045573fcaf348a1df92eb1084bf25ae0a9a1
oserikov <srkvoa@gmail.com>
2023-06-13 04:00:54 +0300

    removed mongo from app.py, found data

--- app.py
+++ app.py
@@ -1,18 +1,14 @@
 from flask import Flask
 from flask import render_template
-from pymongo import MongoClient
+import pandas as pd
 import json
-from bson import json_util
-from bson.json_util import dumps
+import random
 
 app = Flask(__name__)
 
-MONGODB_HOST = 'localhost'
-MONGODB_PORT = 27017
-DBS_NAME = 'donorschoose'
-COLLECTION_NAME = 'projects'
-FIELDS = {'school_state': True, 'resource_type': True, 'poverty_level': True, 'date_posted': True, 'total_donations': True, '_id': False}
-
+fname = "projects_donations.csv"
+data_df = pd.read_csv(fname)
+data = data_df.to_dict(orient="records")
 
 @app.route("/")
 def index():
@@ -21,16 +17,7 @@ def index():
 
 @app.route("/donorschoose/projects")
 def donorschoose_projects():
-    connection = MongoClient(MONGODB_HOST, MONGODB_PORT)
-    collection = connection[DBS_NAME][COLLECTION_NAME]
-    projects = collection.find(projection=FIELDS, limit=100000)
-    #projects = collection.find(projection=FIELDS)
-    json_projects = []
-    for project in projects:
-        json_projects.append(project)
-    json_projects = json.dumps(json_projects, default=json_util.default)
-    connection.close()
-    return json_projects
+    return random.choices(data, k=500)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',port=5000,debug=True)
